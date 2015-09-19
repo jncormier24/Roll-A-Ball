7f01933f44c44a73dca9296627b4da616b394faf
Joe Cormier <jncormier24@gmail.com>
2015-09-19 09:28:34 -0400

    Initial Commit
    Initial Commit

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	void Update (){
+
+	}
+
+	//for physics
+	void FixedUpdate (){
+		float moveHorizontal 	= Input.GetAxis ( "Horizontal" );
+		float moveVertical 		= Input.GetAxis ( "Vertical" );
+
+		//vector (x axis, y axis, z axis)
+		Rigidbody.AddForce(  );
+	}
+}
